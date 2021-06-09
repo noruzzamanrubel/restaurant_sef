2a976d4fda320ab37a9e000ef1cfc892a5e5f255
noruzzamanrubel <noruzzamanrubel@gmail.com>
2021-06-09 15:08:59 +0600

    html design

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,61 @@
 (function ($) {
     $(document).ready(function () {
 
-        const m = moment();
+        // const now = moment();
+        // const today = moment();
 
-        console.log(m.format("dddd"));
+        // $('.day').append(m.format("dddd"));
+        // let resultDates = [];
+        // const getday = m.add(30, 'days');
+        // console.log(getday);
 
-        console.log(m.format("DD/MM/YY"));
+        // var date = new Date();
+        // const getdate = date.setDate(date.getDate() - 30);
+        // console.log(getdate);
+
+        // var daysDate = [];
+        // for(var i = 1; i<= 30; i++) {
+        //     const dates = moment().add(30, 'days');
+        //     daysDate.push( dates._locale._weekdays );
+        // }
+
+        // daysDate.map( (value) => {
+        //     console.log(value)
+        //     $('.weak').append('<div class="col day">'+value+'</div>');
+        // } )
+        // const current = moment();
+        // let n = 7;
+        // while (n > 0) {
+        //     resultDates.push(current.format("dddd"))
+        //     current.subtract(1, "day")
+        //     n--;
+        // }
+
+        // resultDates.map( (value) => {
+        //     console.log(value)
+        //     $('.weak').append('<div class="col day">'+value+'</div>');
+        // } )
+
+        //         var currentDate = moment();
+        // var futureMonth = moment(currentDate).add(1, 'M');
+        // var futureMonthEnd = moment(futureMonth).endOf('month');
+
+        // if(currentDate.date() != futureMonth.date() && futureMonth.isSame(futureMonthEnd.format('YYYY-MM-DD'))) {
+        //     futureMonth = futureMonth.add(1, 'd');
+        // }
+
+        // console.log(currentDate);
+        // console.log(futureMonth);
+
+        // for (i = 1; i < 31; i++) {
+        //     startdate = today.format("DD-MM-YY");
+        //     var new_date = moment(startdate, "DD-MM-YY").add('days', i);
+
+        //     var day = new_date.format('DD');
+        //     var month = new_date.format('MM');
+        //     var year = new_date.format('YY');
+        //     console.log(day)
+        // }
 
     })
 })(jQuery);
