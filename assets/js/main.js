c909714597f1a6359740fe31351458ef9e2677e1
noruzzamanrubel <noruzzamanrubel@gmail.com>
2021-06-12 11:39:16 +0600

    previous day dynamic

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,25 @@
-(function($) {
-    $(document).ready(function() {
+(function ($) {
+    $(document).ready(function () {
         var today = moment().subtract(1, 'day');
+        var yes = moment().subtract(1, 'day');
 
         renderNextSevenDays(today);
 
-        $('.availability_shedule_right_arrow').click(function() {
+        $('.availability_shedule_left_arrow').click(function () {
+            for (var i = 7; i >= 1; i--) {
+                yes.subtract(1, 'days');
+                var day = yes.format('dddd');
+                var date = yes.format('DD/MM/YYYY');
+                $('#day_' + i + '_day').text(day);
+                $('#day_' + i + '_date').text(date);
+            }
+        });
+
+        $('.availability_shedule_right_arrow').click(function () {
             renderNextSevenDays(today);
         });
 
-        $('.availability_switch').click(function() {
+        $('.availability_switch').click(function () {
             var items = $(this).closest('.availability_content').children('.availability_items');
 
             if ($(this).prop('checked')) {
@@ -20,13 +31,13 @@
     });
 
     function renderNextSevenDays(today) {
-        for (var i=1; i<=7; i++) {
+        for (var i = 1; i <= 7; i++) {
             today.add(1, 'days');
             var day = today.format('dddd');
-            var date = today.format('DD/MM/YYYY')
+            var date = today.format('DD/MM/YYYY');
 
-            $('#day_'+i+'_day').text(day);
-            $('#day_'+i+'_date').text(date);
+            $('#day_' + i + '_day').text(day);
+            $('#day_' + i + '_date').text(date);
         }
     }
 })(jQuery);
